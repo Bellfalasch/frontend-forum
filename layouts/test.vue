f5a8a43dc1b3715124a5bce6884c1774f817bc53
Bobby Westberg <bellfalasch@gmail.com>
2024-01-16 14:50:37 +0100

    fix: move inline-style to custom.css

--- layouts/test.vue
+++ layouts/test.vue
@@ -1,7 +1,6 @@
 <!-- test.vue -->
 <template>
-  <div class="slidev-layout default test" style="background-color: #161c2c;
-        background: linear-gradient(to right, #4947b7, #9c3750);">
+  <div class="slidev-layout default test">
     <slot />
   </div>
 </template>
