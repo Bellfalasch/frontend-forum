6ed63b3ccdbed1df1c7050babc42415658ca5d3f
Bellfalasch <bellfalasch@gmail.com>
2024-08-28 14:14:42 +0200

    chore: archive todays FEF

--- test.js
+++ test.js
@@ -3,16 +3,17 @@ javascript:(function() {
     if (metaID) { 
         var rootUrl = "gjensidige-builders-admin.gjensidige.io";
         var currentUrl = window.location.href;
-        if (currentUrl.substring(8,45) === "gjensidige-builders.testgjensidige.io") { rootUrl = "gjensidige-builders-admin.apps-int.testgjensidige.io"; }
-        if (currentUrl.substring(8,39) === "gjensidige-no.testgjensidige.io") { rootUrl = "gjensidige-no-admin.apps-int.testgjensidige.io"; }
-        if (currentUrl.substring(8,39) === "gjensidige-se.testgjensidige.io") { rootUrl = "gjensidige-se-admin.apps-int.testgjensidige.io"; }
-        if (currentUrl.substring(8,39) === "gjensidige-dk.testgjensidige.io") { rootUrl = "gjensidige-dk-admin.apps-int.testgjensidige.io"; }
-        if (currentUrl.substring(8,40) === "gjensidige-com.testgjensidige.io") { rootUrl = "gjensidige-com-admin.apps-int.testgjensidige.io"; }
-        if (currentUrl.substring(8,25) === "www.gjensidige.no") { rootUrl = "gjensidige-no-admin.apps-int.gjensidige.io"; }
-        if (currentUrl.substring(8,25) === "www.gjensidige.se") { rootUrl = "gjensidige-se-admin.apps-int.gjensidige.io"; }
-        if (currentUrl.substring(8,25) === "www.gjensidige.dk") { rootUrl = "gjensidige-dk-admin.apps-int.gjensidige.io"; }
-        if (currentUrl.substring(8,26) === "www.gjensidige.com") { rootUrl = "gjensidige-com-admin.apps-int.gjensidige.io"; }
-        var targetUrl = "https://" + rootUrl + "/admin/tool/com.enonic.app.contentstudio/main/default/edit/";
+        var testServer = true;
+        if (currentUrl.substring(8,25) === "gjensidige-builde") { rootUrl = "builders"; }
+        if (currentUrl.substring(8,25) === "gjensidige-no.tes") { rootUrl = "no"; }
+        if (currentUrl.substring(8,25) === "gjensidige-se.tes") { rootUrl = "se"; }
+        if (currentUrl.substring(8,25) === "gjensidige-dk.tes") { rootUrl = "dk"; }
+        if (currentUrl.substring(8,25) === "gjensidige-com.te") { rootUrl = "com"; }
+        if (currentUrl.substring(8,25) === "www.gjensidige.no") { rootUrl = "no"; testServer = false; }
+        if (currentUrl.substring(8,25) === "www.gjensidige.se") { rootUrl = "se"; testServer = false; }
+        if (currentUrl.substring(8,25) === "www.gjensidige.dk") { rootUrl = "dk"; testServer = false; }
+        if (currentUrl.substring(8,25) === "www.gjensidige.co") { rootUrl = "com"; testServer = false; }
+        var targetUrl = "https://gjensidige-" + rootUrl + "-admin.apps-int." + testServer ? "test" : "" + "gjensidige.io/admin/tool/com.enonic.app.contentstudio/main/default/edit/";
         window.open(targetUrl + metaID)
     }
 })();
