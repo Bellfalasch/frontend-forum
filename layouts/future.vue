391b3fcac1fada1f367fd8772618ba391da7f22a
Bellfalasch <bellfalasch@gmail.com>
2024-12-18 13:00:47 +0100

    fix: add future layout and styles

---
+++ layouts/future.vue
@@ -0,0 +1,16 @@
+<!-- future.vue -->
+<template>
+  <div class="slidev-layout future">
+    <div class="bg-animation">
+      <div id="stars"></div>
+      <div id="stars2"></div>
+      <div id="stars3"></div>
+      <div id="stars4"></div>
+    </div>
+
+    <div class="overlay">
+      <slot />
+      <twemoji-rocket class="spaceship"/>
+    </div>
+  </div>
+</template>
